a12b4e52a5966737d03a5f5ba2f86bead6f4b614
Andrey2008ed <117944057+Andrey2008ed@users.noreply.github.com>
2024-02-14 12:14:11 +0300

    Add files via upload

---
+++ web_project/web_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('info/<int:order_id>/', views.info, name='info')
+
+]
